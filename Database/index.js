5f523c60d45992cfd42e220663ff7afce2b803f5
aframson <obiriaframson@gmail.com>
2022-12-13 18:06:43 +0000

    added mysql

--- Database/index.js
+++ Database/index.js
@@ -4,24 +4,22 @@
 const  mysql = require('mysql')
 const  Config = require('../Config')
 // Railway CONFIGURATION
-let connection = mysql.createConnection({
+let connection = mysql.createPool({
     host: Config.CONNECTION_URL,
     user: Config.USER,
     port: Config.PORT,
     password: Config.PASSWORD,  
     database: Config.DB_NAME
-  });
-  
-
-connection.connect(error => {
-    if (error) {
-      console.log('❌❌ Database error , Oops could not connect to the mysql ')
-      console.log('❌❌ database kindly check the config file to configure  setings ')
-      return
-    } else { 
-      console.log('⚡ 🌿 ✅ Database connected successfully')
-    }
-  });
+});
+// connection.connect(error => {
+//     if (error) {
+//       console.log('❌❌ Database error , Oops could not connect to the mysql ')
+//       console.log('❌❌ database kindly check the config file to configure  setings ')
+//       return
+//     } else { 
+//       console.log('⚡ 🌿 ✅ Database connected successfully')
+//     }
+//   });
 
 
 module.exports = connection;
--- EndPoints/index.js
+++ EndPoints/index.js
@@ -9,8 +9,26 @@ const connection = require('../Database/index')
 
 module.exports = () => {
 
-    router.get('/add',basicAuth, async (req, res) => {
-           res.json({message:"this is for adding"})
+    router.post('/add',basicAuth, async (req, res) => {
+
+          const {title,body,date,update} = req.body
+
+           const query = `INSERT INTO notes (title,body,date,dateUpdate) VALUES ('${title}','${body}','${date}','${update}')`
+   
+           connection.query(query, function (error, results) {
+               if (error) {
+                   res.status(404).json({
+                    status:false,
+                    error:error
+                })
+               } else {
+                   res.status(200).json({
+                       status:true,
+                       message: "Data inserted successfully",
+                       data:results
+                   })
+               }
+           });
     });
 
     return router;
